c0a84fd3cb8d0ed2a002c34169992eaa37ad9ebc
darrenmoschellajr <darrenmoschellajr@gmail.com>
2016-05-16 06:11:23 +0000

    211am

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,14 +68,8 @@ namespace :deploy do
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
+      load 'lib/deploy/seed'
     end
   end
 
-end
-
-namespace :deploy do
-  desc "reload the database with seed data"
-  task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
-  end
 end
